de0ea5fd7074adf3a94c8f54f9f5d6fa27fc0de9
hajera yasin <hajeray61@gmail.com>
2026-05-14 16:20:30 +0500

    Create Basic program.cpp

---
+++ Chapter 2-Variables-and-Datatypes/Basic program.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age = 18;
+    float marks = 89.5;
+    char grade = 'A';
+
+    cout << "Age: " << age << endl;
+    cout << "Marks: " << marks << endl;
+    cout << "Grade: " << grade << endl;
+
+    return 0;
+}
